d5eb183466a6847b565232ecc04377ed75ba0351
David Christiansen <dave@developertown.com>
2013-07-23 18:54:18 -0400

    Bootstrap app with custom pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Selfstarter::Application.routes.draw do
-  root :to => 'preorder#index'
-  match '/preorder'               => 'preorder#index'
+  root :controller => 'home', :action => 'index'
+  match "competition-details" => 'home#competition-details'
+  match "faq" => 'home#faq'
+  match "contact" => 'home#contact'
+
+  match '/hero'               => 'preorder#index'
   get 'preorder/checkout'
   match '/preorder/share/:uuid'   => 'preorder#share', :via => :get
   match '/preorder/ipn'           => 'preorder#ipn', :via => :post
